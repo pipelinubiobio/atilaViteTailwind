ca1f0f56a556b0fcd09633a7175627315a3fa0ad
PipelinUBB <felipe.cifuentes2101@alumnos.ubiobio.cl>
2025-06-17 16:25:08 -0400

    login backend working

---
+++ atilaBackend/src/routes/index.routes.js
@@ -0,0 +1,11 @@
+"use strict";
+import { Router } from "express";
+import userRoutes from "./user.routes.js";
+import authRoutes from "./auth.routes.js";
+
+const router = Router();
+
+router
+    .use("/auth", authRoutes)
+    .use("/user", userRoutes)
+export default router;
---
+++ atilaBackend/src/routes/user.routes.js
@@ -0,0 +1,25 @@
+"use strict";
+import { Router } from "express";
+import { isAdmin } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+  
+} from "../controllers/user.controller.js";
+
+const router = Router();
+
+router
+  .use(authenticateJwt)
+  //.use(isAdmin); comente esto pq no me funcionaba el registro de hora :/
+
+router
+  .get("/", getUsers)
+  .get("/detail/", getUser)
+  .patch("/detail/", updateUser)
+  .delete("/detail/", deleteUser);
+
+export default router;
